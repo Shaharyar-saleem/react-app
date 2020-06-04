9872ab48965b869cc0bf2c93be2a9f019c764713
Shaharyar-saleem <43236957+Shaharyar-saleem@users.noreply.github.com>
2020-06-04 18:27:11 +0500

    updated heading in the main page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world from all of US</h1>
+      <h1>Hello world from all of US Hello pakistan</h1>
     </div>
   );
 }
