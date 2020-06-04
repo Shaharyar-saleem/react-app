0457ab85132a836daf247702e89b154529b1eed5
Shaharyar-saleem <43236957+Shaharyar-saleem@users.noreply.github.com>
2020-06-04 15:24:35 +0500

    updated heading

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world from Shaharyar</h1>
+      <h1>Hello world from all of US</h1>
     </div>
   );
 }
